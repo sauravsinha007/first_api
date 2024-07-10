7e01bed6a014f1c449cca1a606391f88afe2e16b
saurav sinha <sauravsinha007@gmail.com>
2024-07-10 11:01:58 +0530

    Implement docker

--- app.py
+++ app.py
@@ -22,68 +22,62 @@ db = ItemDatabase()
 
 # http://127.0.0.1:5000/get-items
 
-@app.route("/")
-def index():
-  return jsonify({'message': 'Hello, First API World! init itemDb object'})
+# @app.route("/")
+# def index():
+#   return jsonify({'message': 'Hello, First API World! init itemDb object'})
 
-# get method
-# @app.get('/get-items')
-# def get_items():
-#     return db.getItemsFromDb()
-#     #return {"items" : items}
+app = Flask(__name__)
 
-# #Dynamic url
-# @app.get('/get-item/<string:name>')
-# def get_item(name):
-#     return db.getItemFromDb(name)
+#get method
+@app.get('/get-items')
+def get_items():
+    return db.getItemsFromDb()
+    #return {"items" : items}
 
-# # Post 
-# @app.post('/add-item')
-# def add_item():
-#     request_data = request.get_json()
-#     unique_id = uuid.uuid4()
-#     print("request_data = ",request_data)
-#     if db.addItemInDb(unique_id,request_data) == True:
-#        return {"message": "item added sucessfully"}
-#     else:
-#        return {"message": "no row effected"} 
+#Dynamic url
+@app.get('/get-item/<string:name>')
+def get_item(name):
+    return db.getItemFromDb(name)
 
-# @app.post('/update-item')
-# def update_item():
-#     request_data = request.get_json()
-#     print("request_data = ",request_data)
-#     if db.updateItemInDb(request_data) == True:
-#        return {"message": "item updated sucessfully"}
-#     else:
-#        return {"message": "no row effected"}  
+# Post 
+@app.post('/add-item')
+def add_item():
+    request_data = request.get_json()
+    unique_id = uuid.uuid4()
+    print("request_data = ",request_data)
+    if db.addItemInDb(unique_id,request_data) == True:
+       return {"message": "item added sucessfully"}
+    else:
+       return {"message": "no row effected"} 
+
+@app.post('/update-item')
+def update_item():
+    request_data = request.get_json()
+    print("request_data = ",request_data)
+    if db.updateItemInDb(request_data) == True:
+       return {"message": "item updated sucessfully"}
+    else:
+       return {"message": "no row effected"}  
     
     
 
-# @app.post('/delete-item')
-# def delete_item():
-#     request_data = request.get_json()
-#     print("request_data = ",request_data)
-#     if db.deleteItemFromDb(request_data) == True:
-#        return {"message": "item deleted sucessfully"}
-#     else:
-#        return {"message": "no row effected"}
+@app.post('/delete-item')
+def delete_item():
+    request_data = request.get_json()
+    print("request_data = ",request_data)
+    if db.deleteItemFromDb(request_data) == True:
+       return {"message": "item deleted sucessfully"}
+    else:
+       return {"message": "no row effected"}
 
-#     return {"message":"Item not exist"}
+    return {"message":"Item not exist"}
 
-# # Query Parameter
-# @app.get('/get-item-query-parameter')
-# def get_item_query_parameter():
-#     name = request.args.get('name')
-#     for item in items:
-#         if item["name"] == name:
-#             return {"item" : item}
+# Query Parameter
+@app.get('/get-item-query-parameter')
+def get_item_query_parameter():
+    name = request.args.get('name')
+    for item in items:
+        if item["name"] == name:
+            return {"item" : item}
         
-#     return {"message":"Item not exist"}
-
-# # Post 
-# @app.post('/add-item')
-# def add_item():
-#     request_data = request.get_json()
-#     #print(request_data)
-#     items.append(request_data)
-#     return {"message": "item added sucessfully"}
+    return {"message":"Item not exist"}
